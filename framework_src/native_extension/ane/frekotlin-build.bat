a2854b88c04808a5a9288b912b39ed2d3020633e
Eoin Landy <el111@yahoo.com>
2017-12-30 23:44:06 +0000

    1.0.0

--- framework_src/native_extension/ane/frekotlin-build.bat
+++ framework_src/native_extension/ane/frekotlin-build.bat
@@ -1,7 +1,7 @@
 @echo off
 SET pathtome=%~dp0
 SET SZIP="C:\Program Files\7-Zip\7z.exe"
-SET AIR_PATH="D:\dev\sdks\AIR\AIRSDK_27\bin\"
+SET AIR_PATH="D:\dev\sdks\AIR\AIRSDK_28\bin\"
 
 SET KOTLIN_VERSION="1.2.10"
 SET PROJECT_NAME=FreKotlinExampleANE
